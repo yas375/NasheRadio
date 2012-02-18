bc47a17723685ddfda41ce89543151100bc5cb7d
yas <ilyukevich.victor@gmail.com>
2012-02-18 14:01:51 +0300

    Initial Commit

---
+++ Nashe/NRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRAppDelegate.h
+//  Nashe
+//
+//  Created by Victor Ilyukevich on 18.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nashe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nashe
+//
+//  Created by Victor Ilyukevich on 18.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRAppDelegate class]));
+    }
+}
